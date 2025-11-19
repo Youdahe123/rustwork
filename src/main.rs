ac4b3e4dedd70a6f3743a66dc047ccd3b9640fc2
Youdahe Asfaw <youdaheasfaw@gmail.com>
2025-11-19 17:33:27 -0600

    while loops :P

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 fn main() {
     let secNumber = rand::thread_rng().gen_range(1..=10);
-    let mut messageToLow = String::new();
-    let mut messageCorrect = String::new();
 
-    println!("Correct Number -> {}", secNumber);
+    // let mut messageCorrect = String::new();
+
     println!("Hey Guess a Number 1-10");
-    let mut num = String::new();
-    io::stdin()
-        .read_line(&mut num)
-        .expect("Error Reading input");
-    let num: u32 = num.trim().parse().expect("Please Enter Number");
-    if secNumber < num {
-        println!("Your guess is to high try to go lower")
-    } else if secNumber > num {
-        println!("Your guess is to low try to go higher")
-    } else {
-        println!("Congrats your correct!")
-    }
-    let mut messageToHigh = if secNumber < num { String::from("Your Number was to High");}
-}
 
+    let mut how_many = String::new();
+    println!("How many random numbers do you want to");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Error reading input");
+    let numGuess: u8 = how_many.trim().parse().expect("Error");
+
+    let mut correct = Vec::new();
+
+    for i in 0..numGuess {
+        correct.push(rand::thread_rng().gen_range(1..=10));
+    }
+    println!("{correct:?}");
+
+    let mut guessMade = 0;
+
+    while guessMade < numGuess {
+        let mut num = String::new();
+        io::stdin()
+            .read_line(&mut num)
+            .expect("Error Reading input");
+
+        let num: u32 = match num.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("This is the error");
+                continue;
+            }
+        };
+
+        let message = match num.cmp(&correct[guessMade as usize]) {
+            Ordering::Greater => "Your Number was to High",
+            Ordering::Less => "Your Number was to low",
+            Ordering::Equal => {
+                guessMade += 1;
+                "Congrats Your Finally Correct"
+            }
+        };
+        println!("{message}");
+    }
+}
 // worked on mut and some basic if and else statementss - 11/18
